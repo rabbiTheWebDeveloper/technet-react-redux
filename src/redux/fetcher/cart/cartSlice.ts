a257d11b69fe0140c582bb9bda504c1a66ff2816
S M AL RABBI <rabbithedeveloper@gmail.com>
2023-07-11 17:57:43 +0600

    Update cartSlice.ts

--- src/redux/fetcher/cart/cartSlice.ts
+++ src/redux/fetcher/cart/cartSlice.ts
@@ -1,7 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { IProduct } from '@/types/globalTypes';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
-const initialState: CounterState = {
-    product : 0,
-  }
+interface ICart {
+  products: IProduct[];
+  total: number;
+}
+const initialState: ICart = {
+  products: [],
+  total: 0,
+};
 
-  const 
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action :PayloadAction<IProduct>) => {
+      state.products.push(action.payload);
+    }
+  },
+});
+
+export default cartSlice.reducer;
